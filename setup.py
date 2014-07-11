ce5fd2c716c05e07dea76e813f6190ef84c9ab21
Will Daly <will@edx.org>
2014-07-11 13:48:54 -0400

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-submissions',
-    version='0.0.2',
+    version='0.0.3',
     author='edX',
     description='An API for creating submissions and scores.',
     url='http://github.com/edx/edx-submissions.git',

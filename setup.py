d6a14f5d738fce559656af5ea081b9aa39d9c2ac
Dave St.Germain <dstgermain@edx.org>
2014-09-16 12:45:52 -0400

    Version 0.0.8

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-submissions',
-    version='0.0.7',
+    version='0.0.8',
     author='edX',
     description='An API for creating submissions and scores.',
     url='http://github.com/edx/edx-submissions.git',

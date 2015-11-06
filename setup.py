51e5b8c26f7d78eebf1d8f7ed36575216aa20f55
muhammad-ammar <mammar@gmail.com>
2015-11-06 12:26:37 +0500

    bump library version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-submissions',
-    version='1.0',
+    version='0.1.1',
     author='edX',
     description='An API for creating submissions and scores.',
     url='http://github.com/edx/edx-submissions.git',

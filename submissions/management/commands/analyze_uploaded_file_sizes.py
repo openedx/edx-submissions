64a37224378879684325171ca36aa5b846109102
Jansen Kantor <jkantor@edx.org>
2019-11-18 12:17:47 -0500

    Jkantor/mgmt (#102)
    * management command to check uploaded file sizes

---
+++ submissions/management/commands/analyze_uploaded_file_sizes.py
@@ -0,0 +1,169 @@
+"""
+Management command to analyze recently-uploaded files and create a report containing:
+
+Per course:
+    1) total bytes of files uploaded
+    2) count of users who have uploaded a file
+    3) rough bytes-per-user, #1 divided by #2
+
+This report is printed to stdout with a header and values as a tabbed stream.
+
+Arguments:
+    -max_date (optional): The most recent day to include in the report. Defaults to today.
+    -min_date (optional): The longest-ago day to include in the report.
+                          Defaults to <today - 30 days>.
+                          The maximum value for this argument is 2019-11-01.
+                          Before this day, file size data is unavailable.
+
+    both arguments must be in the form yyyy-mm-dd
+    The two dates cannot be more than 90 days apart.
+"""
+
+from __future__ import absolute_import
+
+import datetime
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+
+from submissions.models import Submission
+
+DATE_FORMAT = '%Y-%m-%d'
+EARLIEST_ALLOWED_DATE = datetime.date(2019, 11, 1)
+EARLIEST_ALLOWED_DATE_STR = EARLIEST_ALLOWED_DATE.strftime(DATE_FORMAT)
+HEADER = u'Course ID\tUsers with Uploaded Files\tTotal Uploaded Bytes\tAverage Upload per User'
+
+
+class Command(BaseCommand):
+    """
+    Example usage:
+
+        ./manage.py lms analyze_uploaded_file_sizes --max_date=2020-01-30 --min_date=2019-12-31
+
+    """
+    help = u'Collects and prints stats about ORA file upload usage'
+
+    def add_arguments(self, parser):
+        """
+        Add arguments to the command parser.
+        """
+        today = datetime.date.today()
+        thirty_days = datetime.timedelta(days=30)
+        thirty_days_ago = today - thirty_days
+
+        def parse_date(s):
+            datetime.datetime.strptime(s, DATE_FORMAT).date()
+
+        parser.add_argument(
+            '--min_date',
+            type=parse_date,
+            default=thirty_days_ago,
+            help=(u"The longest-ago day to include in the report. Defaults to <today - 30 days>. "
+                  u"Cannot be set to before 2019-11-01.")
+        )
+        parser.add_argument(
+            '--max_date',
+            type=parse_date,
+            default=today,
+            help=u"The most recent day to include in the report. Defaults to today."
+        )
+
+    # pylint: disable=arguments-differ, unused-argument
+    def handle(self, min_date, max_date, *args, **options):
+        """
+        Analyze ORA file upload submissions and and print tab-limited report
+        """
+        print(u'Starting file upload submission report')
+        arg_echo_str = u'min_date = {}, max_date = {}'.format(
+            min_date.strftime(DATE_FORMAT),
+            max_date.strftime(DATE_FORMAT)
+        )
+        print(arg_echo_str)
+        min_date = self.validate_input_dates(min_date, max_date)
+        submission_data = self.load_data(min_date, max_date)
+        print(HEADER)
+        for course_row in self.parse_submission_data_by_course(submission_data):
+            course_id, num_users, course_bytes = course_row
+            self.print_row(course_id, num_users, course_bytes)
+
+    def validate_input_dates(self, min_date, max_date):
+        """
+        Checks the validity of min_date and max_date
+        Checks that the end date is not before LAST_DATE, and if it is, overrides it.
+        """
+        if max_date < min_date:
+            raise CommandError("Max date must be less than (before) start date")
+        if max_date - min_date > datetime.timedelta(days=90):
+            raise CommandError("Max date and min date cannot be more than 90 days apart")
+        if datetime.date.today() < max_date:
+            print(u"Warning: max_date is in the future")
+        if min_date < EARLIEST_ALLOWED_DATE:
+            msg = u'File size data is unavailable before {earliest}. Setting min_date to {earliest}'
+            print(msg.format(earliest=EARLIEST_ALLOWED_DATE_STR))
+            return EARLIEST_ALLOWED_DATE
+        return min_date
+
+    def load_data(self, min_date, max_date):
+        """ Load the uploaded file data from the database """
+        # The model fields are datetimes, we need to make sure we
+        # include everything that was submitted in that day
+        min_datetime = self.beginning_of_day(min_date)
+        max_datetime = self.end_of_day(max_date)
+
+        return Submission.objects.filter(
+            created_at__range=(min_datetime, max_datetime),
+            student_item__item_type='openassessment',
+            answer__contains='files_sizes":['
+        ).order_by(
+            'student_item__course_id',
+            'student_item__student_id'
+        ).select_related('student_item').values_list(
+            'student_item__course_id',
+            'student_item__student_id',
+            'answer',
+        )
+
+    def beginning_of_day(self, dt):
+        """ Given a datetime, return a datetime that represents the first second of that day """
+        return datetime.datetime.combine(dt, datetime.time.min)
+
+    def end_of_day(self, dt):
+        """ Given a datetime, return a datetime that represents the last second of that day """
+        return datetime.datetime.combine(dt, datetime.time.max)
+
+    def parse_submission_data_by_course(self, submission_data):
+        """
+        Groups submission_data by course
+
+        Generates:
+            (<course_id>, <number of users with uploaded files>, <total bytes uploaded in course>)
+            for each course
+        """
+        course_bytes = 0
+        users = set()
+        prev_course_id = None
+
+        for course_id, student_id, answer in submission_data:
+            answer = json.loads(answer)
+            if course_id != prev_course_id and prev_course_id is not None:
+                yield prev_course_id, len(users), course_bytes
+                course_bytes = 0
+                users = set()
+            user_submission_bytes = sum(answer['files_sizes'])
+            if user_submission_bytes:
+                users.add(student_id)
+                course_bytes += user_submission_bytes
+            prev_course_id = course_id
+
+        if prev_course_id:
+            yield prev_course_id, len(users), course_bytes
+
+    def print_row(self, course_id, num_course_users, course_bytes):
+        """ Print a row of the report """
+        if num_course_users or course_bytes:
+            print(u"{course_id}\t{num_users}\t{total_bytes}\t{avg_bytes:.0f}".format(
+                course_id=course_id,
+                num_users=num_course_users,
+                total_bytes=course_bytes,
+                avg_bytes=course_bytes/num_course_users if num_course_users != 0 else 0
+            ))
---
+++ submissions/management/commands/tests/test_analyze_uploaded_file_sizes.py
@@ -0,0 +1,296 @@
+"""
+Tests for the analyze_uploaded_file_sizes management command.
+"""
+import ddt
+import mock
+import datetime as dt
+import sys
+try:
+    from cStringIO import StringIO
+except ModuleNotFoundError:
+    from io import StringIO
+from django.core.management import call_command
+from django.core.management.base import CommandError
+from django.test import TestCase
+
+from ..analyze_uploaded_file_sizes import Command, HEADER
+from ....tests.factories import StudentItemFactory, SubmissionFactory
+from ....models import Submission
+import contextlib
+
+
+@contextlib.contextmanager
+def capture():
+    """ Context manager to capture stdout and stderr """
+    oldout, olderr = sys.stdout, sys.stderr
+    try:
+        out = [StringIO(), StringIO()]
+        sys.stdout, sys.stderr = out
+        yield out
+    finally:
+        sys.stdout, sys.stderr = oldout, olderr
+        out[0] = out[0].getvalue()
+        out[1] = out[1].getvalue()
+
+
+class BaseMixin(object):
+
+    def assert_command_output(self, expected_output, max_date=None, min_date=None):
+        """ Run the management command and assert the output """
+        with capture() as out:
+            if not max_date and not min_date:
+                call_command(Command())
+            else:
+                call_command(Command(), max_date=max_date, min_date=min_date)
+        result, _ = self.parse_output(out)
+        self.assertDictEqual(result, expected_output)
+
+    def parse_output(self, out):
+        """
+        Read captured stdout and parse tab limited output table into a dict
+        Returns: (<parsed_table>, <list of any lines before the table>)
+        """
+        lines = out[0].splitlines()
+        header_index = lines.index(HEADER)
+        preamble = lines[:header_index]
+        result = {}
+        for line in lines[header_index+1:]:
+            cells = line.split('\t')
+            result[cells[0]] = (int(cells[1]), int(cells[2]), int(cells[3]))
+        return result, preamble
+
+    def create_answer(self, *filesizes):
+        return {'files_sizes': filesizes}
+
+    def create_another_submission(
+            self,
+            submission,
+            answer=None,
+            same_user=False,
+            same_course=False,
+            time_delta=None,
+    ):
+        """
+        Creates a new Submission, copying certain fields from the 'base'
+        """
+        kwargs = {}
+        if same_user:
+            kwargs['student_id'] = submission.student_item.student_id
+        if same_course:
+            kwargs['course_id'] = submission.student_item.course_id
+        student_item = StudentItemFactory.create(**kwargs)
+        created_at = submission.created_at
+        new_submission = SubmissionFactory.create(
+            student_item=student_item,
+            answer=answer if answer else submission.answer,
+            created_at=created_at if time_delta is None else created_at + time_delta
+        )
+        return new_submission
+
+
+@ddt.ddt
+class TestOutput(BaseMixin, TestCase):
+    """
+    Test that the command correctly processes submissions data
+    """
+
+    def test_no_submissions(self):
+        self.assert_command_output({})
+
+    def test_empty_submission_only(self):
+        SubmissionFactory.create(answer=self.create_answer())
+        self.assert_command_output({})
+
+    def test_one_empty_submission(self):
+        submission_1 = SubmissionFactory.create(answer=self.create_answer())
+        submission_2 = SubmissionFactory.create(answer=self.create_answer(100, 200))
+        self.assert_command_output(
+            {submission_2.student_item.course_id: (1, 300, 300)}
+        )
+
+    @ddt.data([100], [100, 200, 300])
+    def test_one_entry(self, filesizes):
+        submission = SubmissionFactory.create(answer=self.create_answer(*filesizes))
+        total_filesize = sum(filesizes)
+        self.assert_command_output(
+            {submission.student_item.course_id: (1, total_filesize, total_filesize)}
+        )
+
+    def test_one_course_one_learner(self):
+        submission = SubmissionFactory.create(answer=self.create_answer(100, 100, 100))
+        self.create_another_submission(submission, same_user=True, same_course=True)
+        self.create_another_submission(submission, same_user=True, same_course=True)
+        self.assert_command_output(
+            {submission.student_item.course_id: (1, 900, 900)}
+        )
+
+    def test_one_course_multiple_users_one_submission_per(self):
+        submission = SubmissionFactory.create(answer=self.create_answer(100, 100, 100, 200))
+        self.create_another_submission(submission, same_course=True)
+        self.create_another_submission(submission, same_course=True)
+        self.assert_command_output(
+            {submission.student_item.course_id: (3, 1500, 500)}
+        )
+
+    def test_one_course_multiple_users_multiple_submissions(self):
+        user_1_submission_1 = SubmissionFactory.create(answer=self.create_answer(100, 100, 100))
+        submission = user_1_submission_1
+        self.create_another_submission(submission, same_user=True, same_course=True)
+        self.create_another_submission(submission, same_course=True)
+        user_3_submission_1 = self.create_another_submission(submission, same_course=True)
+        self.create_another_submission(user_3_submission_1, same_course=True, same_user=True)
+        self.assert_command_output(
+            {submission.student_item.course_id: (3, 1500, 500)}
+        )
+
+    def test_multiple_courses_one_user(self):
+        submission_1 = SubmissionFactory.create(answer=self.create_answer(100, 100, 100))
+        submission_2 = self.create_another_submission(submission_1, same_user=True)
+        submission_3 = self.create_another_submission(submission_1, same_user=True)
+        submission_4 = self.create_another_submission(submission_1, same_user=True)
+        self.assert_command_output(
+            {
+                submission_1.student_item.course_id: (1, 300, 300),
+                submission_2.student_item.course_id: (1, 300, 300),
+                submission_3.student_item.course_id: (1, 300, 300),
+                submission_4.student_item.course_id: (1, 300, 300),
+            }
+        )
+
+    def test_many_courses(self):
+        """
+        Five courses, each course has 5000 total bytes
+        First course has one user, fifth course has five users
+        """
+
+        # 'makes' ten courses
+        first_submissions = [
+            SubmissionFactory.create(answer=self.create_answer(500, 300, 200)) for _ in range(5)
+        ]
+        for additional_users, base_submission in enumerate(first_submissions):
+            for _ in range(4):
+                self.create_another_submission(
+                    base_submission,
+                    same_course=True,
+                    same_user=additional_users > 0
+                )
+                additional_users -= 1
+
+        expected_output = {
+            submission.student_item.course_id: (5 - i, 5000, round(5000 / (5 - i)))
+            for i, submission in enumerate(first_submissions)
+        }
+        self.assert_command_output(expected_output)
+
+
+@ddt.ddt
+class TestDateRange(BaseMixin, TestCase):
+
+    max_date = dt.date(2020, 1, 30)
+    max_date_str = '2020-01-30'
+    min_date = dt.date(2020, 1, 1)
+    min_date_str = '2020-01-01'
+
+    td_month = dt.timedelta(weeks=4)
+    td_day = dt.timedelta(days=1)
+    td_none = dt.timedelta()
+
+    def test_max_date_before_min_date(self):
+        msg = 'Max date must be less than (before) start date'
+        with self.assertRaisesMessage(CommandError, msg):
+            call_command(Command(), max_date=self.min_date, min_date=self.max_date)
+
+    def test_max_range(self):
+        msg = 'Max date and min date cannot be more than 90 days apart'
+        with self.assertRaisesMessage(CommandError, msg):
+            call_command(Command(), max_date=dt.date(2020, 9, 1), min_date=dt.date(2020, 1, 1))
+
+    @ddt.unpack
+    @ddt.data((True, 600), (False, 900))
+    def test_submission_excluded_same_course_same_user(self, exclude_submission, expected_bytes):
+        submission_1 = SubmissionFactory.create(
+            created_at=dt.datetime(2020, 1, 3),
+            answer=self.create_answer(100, 100, 100)
+        )
+        self.create_another_submission(
+            submission_1,
+            same_course=True,
+            same_user=True,
+            time_delta=self.td_day
+        )
+        self.create_another_submission(
+            submission_1,
+            same_course=True,
+            same_user=True,
+            time_delta=self.td_month if exclude_submission else self.td_day
+        )
+        self.assert_command_output(
+            {submission_1.student_item.course_id: (1, expected_bytes, expected_bytes)},
+            max_date=self.max_date,
+            min_date=self.min_date,
+        )
+
+    @ddt.unpack
+    @ddt.data((True, 2, 600), (False, 3, 900))
+    def test_submission_excluded_same_course_different_user(
+        self, exclude_submission, expected_users, expected_bytes
+    ):
+        submission_1 = SubmissionFactory.create(
+            created_at=dt.datetime(2020, 1, 3),
+            answer=self.create_answer(100, 100, 100)
+        )
+        self.create_another_submission(
+            submission_1,
+            same_course=True,
+            time_delta=self.td_day
+        )
+        self.create_another_submission(
+            submission_1,
+            same_course=True,
+            time_delta=self.td_month if exclude_submission else self.td_day
+        )
+        self.assert_command_output(
+            {submission_1.student_item.course_id: (expected_users, expected_bytes, 300)},
+            max_date=self.max_date,
+            min_date=self.min_date,
+        )
+
+    @ddt.data(True, False)
+    def test_entire_course_out_of_range(self, exclude_course):
+        course_1_submission = SubmissionFactory.create(
+            created_at=dt.datetime(2020, 1, 3),
+            answer=self.create_answer(100, 100, 100)
+        )
+        self.create_another_submission(
+            course_1_submission,
+            same_course=True,
+        )
+        self.create_another_submission(
+            course_1_submission,
+            same_course=True,
+        )
+
+        if exclude_course:
+            created_at = dt.datetime(2019, 12, 1)
+        else:
+            created_at = dt.datetime(2020, 1, 3)
+        course_2_submission = SubmissionFactory.create(
+            created_at=created_at,
+            answer=self.create_answer(500, 200)
+        )
+        self.create_another_submission(
+            course_2_submission,
+            same_course=True,
+        )
+
+        expected_output = {
+            course_1_submission.student_item.course_id: (3, 900, 300)
+        }
+        if not exclude_course:
+            expected_output[course_2_submission.student_item.course_id] = (2, 1400, 700)
+
+        self.assert_command_output(
+            expected_output,
+            max_date=self.max_date,
+            min_date=self.min_date,
+        )

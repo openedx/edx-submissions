8046771b54fae9f75f5cdcb70ab1391c4f6ea66e
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-08-01 14:51:58 +0500

    fix: Added support for django42

--- submissions/admin.py
+++ submissions/admin.py
@@ -16,18 +16,28 @@ class StudentItemAdminMixin:
         'student_item__id'
     )
 
+    @admin.display(
+        ordering='student_item__course_id'
+    )
     def course_id(self, obj):
         return obj.student_item.course_id
-    course_id.admin_order_field = 'student_item__course_id'
 
+    @admin.display(
+        ordering='student_item__item_id'
+    )
     def item_id(self, obj):
         return obj.student_item.item_id
-    item_id.admin_order_field = 'student_item__item_id'
 
+    @admin.display(
+        ordering='student_item__student_id'
+    )
     def student_id(self, obj):
         return obj.student_item.student_id
-    student_id.admin_order_field = 'student_item__student_id'
 
+    @admin.display(
+        description='S.I. ID',
+        ordering='student_item__id',
+    )
     def student_item_id(self, obj):
         """ Formated student item id. """
         url = reverse(
@@ -36,10 +46,9 @@ class StudentItemAdminMixin:
         )
         return format_html(f'<a href="{url}">{obj.student_item.id}</a>')
 
-    student_item_id.admin_order_field = 'student_item__id'
-    student_item_id.short_description = 'S.I. ID'
 
 
+@admin.register(StudentItem)
 class StudentItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'course_id', 'item_type', 'item_id', 'student_id')
     list_filter = ('item_type',)
@@ -47,6 +56,7 @@ class StudentItemAdmin(admin.ModelAdmin):
     readonly_fields = ('course_id', 'item_type', 'item_id', 'student_id')
 
 
+@admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin, StudentItemAdminMixin):
     """ Student Submission Admin View. """
     list_display = (
@@ -83,6 +93,7 @@ class SubmissionInlineAdmin(admin.TabularInline, StudentItemAdminMixin):
     extra = 0
 
 
+@admin.register(TeamSubmission)
 class TeamSubmissionAdmin(admin.ModelAdmin):
     """ Student Submission Admin View. """
 
@@ -92,6 +103,7 @@ class TeamSubmissionAdmin(admin.ModelAdmin):
     inlines = (SubmissionInlineAdmin,)
 
 
+@admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin, StudentItemAdminMixin):
     """ Student Score Admin View. """
     list_display = (
@@ -114,6 +126,7 @@ class ScoreAdmin(admin.ModelAdmin, StudentItemAdminMixin):
         return f"{score.points_earned}/{score.points_possible}"
 
 
+@admin.register(ScoreSummary)
 class ScoreSummaryAdmin(admin.ModelAdmin, StudentItemAdminMixin):
     """ Student Score Summary Admin View. """
     list_display = (
@@ -127,25 +140,21 @@ class ScoreSummaryAdmin(admin.ModelAdmin, StudentItemAdminMixin):
     )
     exclude = ('highest', 'latest')
 
+    @admin.display(
+        description='Highest'
+    )
     def highest_link(self, score_summary):
         url = reverse(
             'admin:submissions_score_change', args=[score_summary.highest.id]
         )
         return format_html(f'<a href="{url}">{score_summary.highest}</a>')
 
-    highest_link.short_description = 'Highest'
 
+    @admin.display(
+        description='Latest'
+    )
     def latest_link(self, score_summary):
         url = reverse(
             'admin:submissions_score_change', args=[score_summary.latest.id]
         )
         return format_html(f'<a href="{url}">{score_summary.latest}</a>')
-
-    latest_link.short_description = 'Latest'
-
-
-admin.site.register(Score, ScoreAdmin)
-admin.site.register(StudentItem, StudentItemAdmin)
-admin.site.register(Submission, SubmissionAdmin)
-admin.site.register(TeamSubmission, TeamSubmissionAdmin)
-admin.site.register(ScoreSummary, ScoreSummaryAdmin)

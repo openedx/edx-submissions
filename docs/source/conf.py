9ad307e4948423af056d6e87f5e74b161f0481fe
Feanil Patel <feanil@axim.org>
2023-08-07 14:48:09 -0400

    docs: Update to be able to pull in docstrings.
    We need to actually setup django and its requirements to be able to
    render autodocs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,14 @@
 
 import os
 import sys
+import django
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+django.setup()
 
 # -- General configuration ------------------------------------------------
 
